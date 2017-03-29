c8ea3cc73662d06a5dbf036dc277991470cee729
Usman Adeel <adeel@citypolyhs.org>
2017-03-29 20:08:01 +0000

    I made a student and teacher picker

--- script.js
+++ script.js
@@ -1,17 +1,39 @@
-$(document).ready(function(){
-  function rolldice() {
-    //Place your code here 
-   
-  }
+/*global $*/
 
-  $("#roll-dice").click(function(){
-    var dice = rolldice();
-    console.log(dice);
-    var ScriptEers = ["Jason", "Barry", "Aldair", "David","Taufiq","Nat","Kyhson"];
-var ScriptEducators = ["Thai", "David", "Michael", "Josh"];
-  var x = Math.floor(Math.random()* (ScriptEers.length-1) );
-    return Math.round(x);
- 
-  });
+// Wait until the webpage is finished loading
+$(document).ready(function(){
   
+  function pickStudent() {
+    //Place your code here 
+    var ScriptEers = [ "Jason", "Barry", "Aldair", "David","Taufiq","Nat","Kyhson"];
+    var x = Math.floor(Math.random()* (ScriptEers.length-1) );
+    
+    return ScriptEers[x];
+  }
+ function pickTeacher() {
+    //Place your code here 
+    var ScriptEducators = ["Thai", "David", "Michael", "Josh"];
+    
+    var x = Math.floor(Math.random()* (ScriptEducators.length-1) );
+    
+    return ScriptEducators[x];
+  }
+  $("#Pick-a-student").click(function(){
+    var student = pickStudent();
+    console.log(student);
+    $("#person").text(student);
+  });
+    $("#Pick-a-teacher").click(function(){
+    var Teacher = pickTeacher();
+    console.log(Teacher);
+    $("#teacher").text(Teacher);
+  });
+// End of wait until webpage is finished loading
 });
+
+
+
+
+
+
+
